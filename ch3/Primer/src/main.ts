452ae85df833e22d3fafdbf36183927aca034e75
Matt Holloway <hollowmatt@gmail.com>
2023-09-04 15:27:12 -0400

    ch4

--- ch3/Primer/src/main.ts
+++ ch3/Primer/src/main.ts
@@ -1,8 +1,10 @@
-let count: number | undefined | null = 100;
-if (count != null && count != undefined) {
-    let result1: string = count.toFixed(2);
-    console.log(`Result 1: ${result1}`);
-}
-
-let result2: string | undefined = count?.toFixed(2);
-console.log(`Result 2: ${result2}`);
+function composeString(val: string) : string  {
+    return `Composed string: ${val}`;
+  }
+  
+  function writeValue(val?: string) {
+    console.log(composeString(val ?? "Fallback value"));
+  }
+  
+  writeValue("London");
+  writeValue();
