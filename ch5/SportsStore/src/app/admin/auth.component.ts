eb1c35adbb9759fc5e3839e72f97b2718ab3feee
Matt Holloway <hollowmatt@gmail.com>
2023-10-05 18:31:41 +0000

    add auth component in new admin folder

---
+++ ch5/SportsStore/src/app/admin/auth.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
+
+@Component({
+    templateUrl: "auth.component.html"
+})
+export class AuthComponent {
+    username?: string;
+    password?: string;
+    errorMessage?: string;
+
+    constructor(private router: Router) {}
+
+    authenticate(form: NgForm) {
+        if (form.valid) {
+            this.router.navigateByUrl("/adming/main");
+        } else {
+            this.errorMessage = "Form Data Invalid";
+        }
+    }
+}
