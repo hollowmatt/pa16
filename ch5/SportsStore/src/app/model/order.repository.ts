94b5bae087d009dff55aa0b749800dbced8edbe2
Matt Holloway <hollowmatt@gmail.com>
2023-10-09 00:06:42 +0000

    update the order repo

--- ch5/SportsStore/src/app/model/order.repository.ts
+++ ch5/SportsStore/src/app/model/order.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable, signal } from "@angular/core";
 import { Observable } from "rxjs";
 import { Order } from "./order.model";
 import { StaticDataSource } from "./static.datasource";
@@ -6,9 +6,44 @@ import { RestDataSource } from "./rest.datasource";
 
 @Injectable()
 export class OrderRepository {
+    private ordersSignal = signal<Order[]>([]);
+    private loaded: boolean = false;
+
     constructor(private dataSource: RestDataSource) {}
 
+    loadOrders() {
+        this.loaded = true;
+        this.dataSource.getOrders().subscribe(data => {
+            this.ordersSignal.set(data);
+        });
+    }
+
+    get orders() {
+        if (!this.loaded) {
+            this.loadOrders();
+        }
+        return this.ordersSignal.asReadonly();
+    }
+
     saveOrder(order: Order): Observable<Order> {
+        this.loaded = false;
         return this.dataSource.saveOrder(order);
+    }
+
+    updateOrder(order: Order) {
+        this.dataSource.updateOrder(order).subscribe(order => {
+            this.ordersSignal.mutate(orders => {
+                orders.splice(orders.findIndex(o => 
+                    o.id == order.id), 1, order);                
+            })
+        });
+    }
+
+    deleteOrder(id: number) {
+        this.dataSource.deleteOrder(id).subscribe(order => {
+            this.ordersSignal.mutate(orders => {
+                orders.splice(orders.findIndex(o => id == o.id), 1);
+            })
+        });
     }
 }
